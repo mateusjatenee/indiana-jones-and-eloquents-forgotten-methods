d0f418019c30a87e6988ad22506bed855f0a3263
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-03 00:01:45 -0300

    examples

--- app/Http/Controllers/TheOnlyController.php
+++ app/Http/Controllers/TheOnlyController.php
@@ -10,6 +10,27 @@ class TheOnlyController extends Controller
     {
         // findMany
         $posts = Post::findMany(['1', '2', '3']);
+
+        return $posts;
+    }
+
+    public function example2()
+    {
+        // firstOrCreate
+        $posts = Post::firstOrCreate([
+            'title' => 'This one doesnt exist',
+            'content' => 'Whatever',
+        ]);
+
+        return $posts;
+    }
+
+    public function example3()
+    {
+        $posts = Post::find(['1', '2', '3'])->each(function ($item, $key) {
+            $item->update(['title' => 'Whatever']);
+        });
+
         return $posts;
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+$router->get('example1', 'TheOnlyController@example1');
+$router->get('example2', 'TheOnlyController@example2');
+$router->get('example3', 'TheOnlyController@example3');
