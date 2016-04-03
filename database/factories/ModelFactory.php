ce7320c850e2fb88529e6d895bb09ca3cd2670e0
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-02 23:31:21 -0300

    post factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,7 +9,7 @@
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
 |
-*/
+ */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
@@ -17,5 +17,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->text,
     ];
 });
